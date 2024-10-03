9389f116814af2b911489955681d76d4f11eb8d4
Jyothikaraj <112985341+Jyothikaraj@users.noreply.github.com>
2024-10-03 09:54:08 -0700

    Update login-script.js

--- login-script.js
+++ login-script.js
@@ -30,7 +30,7 @@ if (name === '' || phoneNumber === '') {
   });
 
   try {
-    const response = await fetch('https://script.google.com/macros/s/AKfycbwrsslFPgMkkw9k8Hu2XSC1-212UZtkc3X5NeV3V1WdZBY1rhZy7JmZB-7W2RshGV0v/exec', {
+    const response = await fetch('https://script.google.com/macros/s/AKfycbwE0BBKayzkZ60GfMJNWYCayfaOLcH-LHXf37TTsXmdUAovTlP5SBFsBz4-cnna3zzp/exec', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded'
